81b67486589050435d3ade2c1967a757f792fa93
qelvyn99 <qelvyn30@gmail.com>
2023-11-08 07:51:42 -0800

    student profile

---
+++ C-programming/Tasks/1-questions.md
@@ -0,0 +1,8 @@
+*Questions:*
+
+Q1. a. What is a variable?
+   b. Provide a real-life example to illustrate the concept of a variable in C.
+
+Q2. Research and provide answers to the following:
+
+   What are the various sizes of different data types on both a 32-bit and a 64-bit computer system?
---
+++ C-programming/Tasks/profile.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(void)
+char Student's Name;
+char Residence;
+int Age;
+float Height(inches);
+char Gender(M or F);
+
+{
+	/*prints the name of the student to the screen*/
+	printf("Student's Name:\n");
+
+	/*prints the place of residence of the student*/
+	printf("Residence:\n");
+
+	/*prints the age of the student*/
+	printf("Age:\n");
+
+	/*prints the height of the student in inches*/
+	printf("Height(in inches):\n");
+
+	/*prints the gender of the student*/
+	printf("Gender(M or F):\n");
+
+	
+	return 0;
+
+}	
+
+
+
+
+
+
+		
