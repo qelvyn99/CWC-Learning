a26f249cd970d9c5fcc1807a66b6136612e162b0
qelvyn99 <qelvyn30@gmail.com>
2023-11-09 03:42:37 -0800

    update on student profile

--- C-programming/Tasks/profile.c
+++ C-programming/Tasks/profile.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 
-int main(void)
-char Student's Name;
-char Residence;
-int Age;
-float Height(inches);
-char Gender(M or F);
+int main(void)/*Entry point of the program*/
 
 {
+	char* Student_Name = "Kelvin Nyantakyi Owusu";
+	char* Residence = "Tantra Hills";
+	int Age = 24;
+	float Height = 5.5;
+	char Gender = 'M';
+
+	/*printing each variable with its own format specifier*/
+	
 	/*prints the name of the student to the screen*/
-	printf("Student's Name:\n");
+	printf("Student's Name:%s\n",Student_Name);
 
 	/*prints the place of residence of the student*/
-	printf("Residence:\n");
+	printf("Residence:%s\n",Residence);
 
 	/*prints the age of the student*/
-	printf("Age:\n");
+	printf("Age:%d\n",Age);
 
 	/*prints the height of the student in inches*/
-	printf("Height(in inches):\n");
+	printf("Height(in inches):%f\n",Height);
 
 	/*prints the gender of the student*/
-	printf("Gender(M or F):\n");
+	printf("Gender(M or F):%c\n",Gender);
 
 	
 	return 0;
