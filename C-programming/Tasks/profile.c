0ff20bf6b7f628f69db77eab5a35402e43766b83
qelvyn99 <qelvyn30@gmail.com>
2023-11-20 15:49:45 -0800

    update

--- C-programming/Tasks/profile.c
+++ C-programming/Tasks/profile.c
@@ -3,27 +3,41 @@
 int main(void)/*Entry point of the program*/
 
 {
-	char *Student_Name = "Kelvin Nyantakyi Owusu";
-	char *Residence = "Tantra Hills";
-	int Age = 24;
-	float Height = 5.5;
-	char Gender = 'M';
+	char *Student_Name[50];
+	char *Residence[50];
+	int Age;
+	float Height;
+	char Gender;
 
-	/*printing each variable with its own format specifier*/
+	/*Adjima's school registration form*/
 	
-	/*prints the name of the student to the screen*/
-	printf("Student's Name:%s\n",Student_Name);
+	
+	printf("This is a school registration form\n");
+	
+	/*takes and prints tye name of the student*/
+	printf("NAME:");
+	scanf("%[^\n]%*c", Student_Name);
+	printf("student's name is %s\n", Student_Name);
 
-	/*prints the place of residence of the student*/
+	/*takes and prints the residence of the student*/
+	printf("RESIDENCE:");
+	scanf("%[^\n]%*c", Residence);
 	printf("Residence:%s\n",Residence);
 
-	/*prints the age of the student*/
+
+	/*takes the age of the student*/
+	printf("Age:");
+	scanf("%d", &Age);
 	printf("Age:%d\n",Age);
 
-	/*prints the height of the student in inches*/
+	/*takes and prints the height of the student in inches*/
+	printf("Height:");
+	scanf("%f", &Height);
 	printf("Height(in inches):%.2f\n",Height);
 
-	/*prints the gender of the student*/
+	/*takes and prints the gender of the student*/
+	printf("Gender:");
+	scanf("%c", &Gender);
 	printf("Gender(M or F):%c\n",Gender);
 
 	
